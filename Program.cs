56b23241a2ee9df7630cdb3b1887cc11491b7301
Ben Alexander <ben250402@yahoo.co.uk>
2025-02-09 18:45:00 +0000

    Changed Program to use blazor properly, and redirect to login

--- Program.cs
+++ Program.cs
@@ -1,27 +1,43 @@
-using Portfolio.Components;
+using Microsoft.AspNetCore.Http.Connections;
+using Portfolio;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+	.AddInteractiveServerComponents();
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+app.UseRouting();
+
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+	.AddInteractiveServerRenderMode();
+
+app.UseStatusCodePagesWithRedirects("/login");
+
+app.MapBlazorHub(options =>
+{
+	options.Transports = HttpTransportType.WebSockets;
+	options.AllowStatefulReconnects = true;
+});
+
+app.MapRazorPages();
 
 app.Run();
