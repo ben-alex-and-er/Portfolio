37e35f12273e8d833cc06b4ca85a1e1f90a15454
Ben Alexander <ben250402@yahoo.co.uk>
2025-03-02 19:57:38 +0000

    Made NotLoggedIn page more generic

---
+++ Components/NotLoggedIn.razor.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Components;
+
+
+namespace Portfolio.Components
+{
+	public partial class NotLoggedIn : ComponentBase
+	{
+		/// <summary>
+		/// Custom message
+		/// </summary>
+		[Parameter]
+		public string? Message { get; set; } = null;
+
+
+		/// <inheritdoc/>
+		protected override void OnInitialized()
+		{
+			Message ??= "You are not logged in.";
+		}
+	}
+}
