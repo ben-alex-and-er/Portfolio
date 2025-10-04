585ebe793076b18ceb7194c250f826ba44111450
Ben Alexander <ben250402@yahoo.co.uk>
2025-10-04 17:30:59 +0100

    10 Added namespace for Program

--- Program.cs
+++ Program.cs
@@ -1,91 +1,96 @@
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
-using Portfolio.Configuration.Database;
-using Portfolio.Configuration.ServiceSetup;
-using Portfolio.Data.Analytics;
-using Portfolio.Data.Api;
-using Portfolio.Data.Generic;
-using Portfolio.DataAccessors.Analytics.Extensions;
-using Portfolio.DataAccessors.Analytics.Interfaces;
-using Portfolio.Services.Api;
-using Portfolio.Services.Api.Interfaces;
 using Radzen;
 
 
-internal class Program
+namespace Portfolio
 {
-	private static void AddServices(IHostApplicationBuilder builder)
+	using Configuration.Database;
+	using Configuration.ServiceSetup;
+	using Data.Analytics;
+	using Data.Api;
+	using Data.Generic;
+	using DataAccessors.Analytics.Extensions;
+	using DataAccessors.Analytics.Interfaces;
+	using Services.Api;
+	using Services.Api.Interfaces;
+
+
+	internal static class Program
 	{
-		builder.Services
-			.AddAnalyticsServices()
-			.AddContextServices()
-			.AddGoogleServices()
-			.AddUserServices();
-
-		var provider = builder.Services.BuildServiceProvider();
-
-		provider.GetRequiredService<Context>().Database.Migrate();
-
-		Task.Run(() => AddEventTypes(provider)).Wait();
-
-
-
-		builder.Services.AddTransient<IApiCallerService, ApiCallerService>();
-
-		builder.Services.AddSingleton<ApiDomain>();
-	}
-
-	private static async Task AddEventTypes(IServiceProvider provider)
-	{
-		var analyticsEventDA = provider.GetRequiredService<IAnalyticsEventDA>();
-
-		await analyticsEventDA.CreateEventTypeOrThrow(new NameGuidDTO(nameof(AnalyticsEventTypes.REGISTER), AnalyticsEventTypes.REGISTER));
-		await analyticsEventDA.CreateEventTypeOrThrow(new NameGuidDTO(nameof(AnalyticsEventTypes.LOGIN), AnalyticsEventTypes.LOGIN));
-	}
-
-	private static void Main(string[] args)
-	{
-		var builder = WebApplication.CreateBuilder(args);
-
-		// Add services to the container.
-		builder.Services.AddRazorComponents()
-			.AddInteractiveServerComponents();
-
-		builder.Services.AddHttpClient();
-
-		AddServices(builder);
-
-		builder.Services.AddRazorPages();
-		builder.Services.AddServerSideBlazor();
-		builder.Services.AddRadzenComponents();
-
-
-		var app = builder.Build();
-
-		if (!app.Environment.IsDevelopment())
+		private static void AddServices(IHostApplicationBuilder builder)
 		{
-			app.UseExceptionHandler("/Error");
-			app.UseHsts();
+			builder.Services
+				.AddAnalyticsServices()
+				.AddContextServices()
+				.AddGoogleServices()
+				.AddUserServices();
+
+			var provider = builder.Services.BuildServiceProvider();
+
+			provider.GetRequiredService<Context>().Database.Migrate();
+
+			Task.Run(() => AddEventTypes(provider)).Wait();
+
+
+
+			builder.Services.AddTransient<IApiCallerService, ApiCallerService>();
+
+			builder.Services.AddSingleton<ApiDomain>();
 		}
 
-		app.UseStaticFiles();
-		app.UseRouting();
-
-		app.UseAntiforgery();
-
-		app.UseForwardedHeaders(new ForwardedHeadersOptions
+		private static async Task AddEventTypes(IServiceProvider provider)
 		{
-			ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-		});
+			var analyticsEventDA = provider.GetRequiredService<IAnalyticsEventDA>();
 
-		app.UseAuthentication();
-		app.UseAuthorization();
+			await analyticsEventDA.CreateEventTypeOrThrow(new NameGuidDTO(nameof(AnalyticsEventTypes.REGISTER), AnalyticsEventTypes.REGISTER));
+			await analyticsEventDA.CreateEventTypeOrThrow(new NameGuidDTO(nameof(AnalyticsEventTypes.LOGIN), AnalyticsEventTypes.LOGIN));
+		}
 
-		app.MapControllers();
-		app.MapBlazorHub();
-		app.MapFallbackToPage("/_Host");
+		private static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-		app.MapRazorPages();
-		app.Run();
+			// Add services to the container.
+			builder.Services.AddRazorComponents()
+				.AddInteractiveServerComponents();
+
+			builder.Services.AddHttpClient();
+
+			AddServices(builder);
+
+			builder.Services.AddRazorPages();
+			builder.Services.AddServerSideBlazor();
+			builder.Services.AddRadzenComponents();
+
+
+			var app = builder.Build();
+
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Error");
+				app.UseHsts();
+			}
+
+			app.UseStaticFiles();
+			app.UseRouting();
+
+			app.UseAntiforgery();
+
+			app.UseForwardedHeaders(new ForwardedHeadersOptions
+			{
+				ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+			});
+
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.MapControllers();
+			app.MapBlazorHub();
+			app.MapFallbackToPage("/_Host");
+
+			app.MapRazorPages();
+			app.Run();
+		}
 	}
 }
