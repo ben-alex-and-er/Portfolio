275c4230a54b57bde50b6ddf1b1dd532d292c3c5
Ben Alexander <ben250402@yahoo.co.uk>
2025-10-21 16:15:28 +0100

    14 Added Orbital Physics Simulator page but needs code explaination

--- Data/Media/YouTubeEmbedConsts.cs
+++ Data/Media/YouTubeEmbedConsts.cs
@@ -9,5 +9,10 @@
 		/// Embed hash for Cave Diver
 		/// </summary>
 		public const string CAVE_DIVER = "iEuvae1T-qQ?si=rInyW-A070FJvVou";
+
+		/// <summary>
+		/// Embed hash for Orbital Physics Simulator
+		/// </summary>
+		public const string ORBITAL_PHYSICS_SIMULATOR = "op0mdyG4oYM";
 	}
 }
